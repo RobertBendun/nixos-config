211f6db2b980fdac77b6218232f30bd992ad2761
Robert Bendun <robert@bendun.cc>
2022-02-18 21:15:20 +0100

    OS configuration

--- link.sh
+++ link.sh
@@ -4,10 +4,19 @@ set -e
 
 C="$(pwd)"
 
-athome() { 
-  ln -vfs "$C/$1" "$HOME/.config/nixpkgs/$1"
+# Mount config file at OS level
+o() { 
+	sudo ln -vfs "$C/os/$1" "/etc/nixos/$1"
 }
 
-athome aliases.nix
-athome config.hs
-athome home.nix
+# Mount config file at home level
+h() { 
+	ln -vfs "$C/$1" "$HOME/.config/nixpkgs/$1" 
+}
+
+h aliases.nix
+h config.hs
+h home.nix
+
+o configuration.nix
+o hardware-configuration.nix
